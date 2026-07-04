47366b20a4c100816508db51cc8e49b8ba4826d8
artem2346 <163147156+artem2346@users.noreply.github.com>
2026-07-04 17:18:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,21 +22,21 @@ print(get_days_from_today('2027-10-09'))
 #2
 
 def get_numbers_ticket(min, max, quantity):
-    if min >= 1 and max <= 1000:
-        random_variables = set([random.randint(min,max) for x in range(quantity)])
-        while True:
-            if len(random_variables) != quantity:
-                random_variables.add(random.randint(min,max))
-            else:
-                break
-        random_choices_variables_sorted = sorted(list(random_variables))
-        return random_choices_variables_sorted
-    else:
+    if min < 1 or max > 1000 or (max - min + 1)  < quantity:
         return []
+    
+    random_variables_unique = set([random.randint(min,max) for x in range(quantity)])
+
+    while len(random_variables_unique) < quantity:
+
+        random_variables_unique.add(random.randint(min,max))
+       
+        
+    random_choices_variables_sorted = sorted(list(random_variables_unique))
+    return random_choices_variables_sorted
 
 
-
-print(get_numbers_ticket(10, 20, 5))
+print(get_numbers_ticket(10, 10, -1))
 
 
 
@@ -44,25 +44,40 @@ print(get_numbers_ticket(10, 20, 5))
 
 
 def normalize_phone(phone_number):
-    delete_gap = phone_number.strip()
-    num = re.sub(r"[^0-9]","",delete_gap)
 
-    if len(num) < 10:
-        return 'Please enter a valid number'
-    
-    elif re.search(r"^0",num):
-        if  len(num) != 10:
-            return 'Please check your number'
+    delete_gap = phone_number.strip()
+    num = re.sub(r"[^0-9+]","",delete_gap)
+
+    if re.search(r"^\+",num) and 7 <= len(num) <= 15:   
+
+        if re.search(r"^\+\d{3}",num).group() == '+380' and len(num) != 13:
+            return 'Please enter a valid number'
         
-        num = re.sub(r"^0","+380",num)
         return num
-    elif re.search(r"^\d{3}",num).group() == '380':
-        if len(num) != 12:
-            return 'Please check your number'
+    
+    elif len(num) == 12 and re.search(r"^\d{3}",num).group() == '380':
         num = re.sub(r"^\d{3}","+380",num)
         return num
+    
+    elif  len(num) == 10 and re.search(r"^0",num):
+        num = re.sub(r"^0","+380",num)
+        return num
+    
     else:
         return 'Please enter a valid number'
+
+
+print(normalize_phone("   +38(050)111-22-11   ")) 
+print(normalize_phone('+3805012345673'))            
+print(normalize_phone('0501234567'))                
+print(normalize_phone('+331234567890'))             
+print(normalize_phone('+380'))                       
+print(normalize_phone('+79'))      
+print(normalize_phone('+38050123456'))   
+print(normalize_phone("48-12"))                
+print(normalize_phone("481234567890"))        
+print(normalize_phone("+86123456"))       
+
 
 
 raw_numbers = [
@@ -76,9 +91,9 @@ raw_numbers = [
     "38050-111-22-22",
     "38050 111 22 11   ",
 ]
-print(normalize_phone('ldlld23453'))
 sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
 print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
+
 
 
 
